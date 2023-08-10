c15a1726f09bc41c9bfd62295d412f050301f52f
User083 <97461050+User083@users.noreply.github.com>
2023-08-10 12:31:41 +0100

    Form Options
    - Adds form options for image amount and resolution
    - Adds a loader component

--- components/GenerationForm.tsx
+++ components/GenerationForm.tsx
@@ -1,5 +1,13 @@
 "use client";
 import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useState } from "react";
+import Loader from "./Loader";
+import { useRouter } from "next/navigation";
+import {
+  amountOptions,
+  resolutionOptions,
+} from "@/app/(dashboard)/(routes)/dashboard/constants";
 
 interface IProps {
   prompt: string;
@@ -8,40 +16,90 @@ interface IProps {
 }
 
 const GenerationForm = () => {
+  const router = useRouter();
   const form = useForm({
     defaultValues: { prompt: "", amount: "1", resolution: "256x256" },
   });
   const isLoading = form.formState.isSubmitting;
+  const [images, setImages] = useState<string[]>([]);
+
   const onSubmit = async (values: IProps) => {
-    console.log(values);
+    try {
+      setImages([]);
+      const response = await axios.post("/api/image", values);
+      const urls = response.data.map((image: { url: string }) => image.url);
+      setImages(urls);
+      form.reset();
+    } catch (error) {
+    } finally {
+      router.refresh();
+    }
   };
 
   return (
     <>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="rounded-lg border w-full p-4 px-3 md:px6 flex"
+        className="rounded-lg border w-full p-4 px-3 md:px6 flex flex-col gap-3 items-center"
       >
-        <input
-          name="prompt"
-          type="text"
-          className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent text-white bg-black w-full"
-          aria-label="Prompt input"
-          disabled={isLoading}
-          placeholder="What does your heart desire?"
-          onChange={() => {
-            console.log(form.getValues(), "VALUES");
-          }}
-        />
-        <button
-          name="Submit"
-          className="p-2 hover:text-gray-400"
-          aria-label="Submit prompt"
-          disabled={isLoading}
-        >
-          Generate
-        </button>
+        <section className="flex gap-5 ">
+          <h3>Options</h3>
+          <select
+            aria-label="Amount"
+            disabled={isLoading}
+            name="Amount"
+            className="bg-black text-white"
+          >
+            {amountOptions.map((amount) => (
+              <option
+                label={amount.label}
+                value={amount.value}
+                key={amount.value}
+              />
+            ))}
+          </select>
+          <select
+            aria-label="Resolution"
+            disabled={isLoading}
+            name="Resolution"
+            className="bg-black text-white"
+          >
+            {resolutionOptions.map((resolution) => (
+              <option
+                key={resolution.value}
+                label={resolution.label}
+                value={resolution.value}
+              />
+            ))}
+          </select>
+        </section>
+        <section className="w-full flex">
+          <input
+            name="prompt"
+            type="text"
+            className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent text-white bg-black w-full"
+            aria-label="Prompt input"
+            disabled={isLoading}
+            placeholder="What does your heart desire?"
+          />
+          <button
+            name="Submit"
+            className="p-2 hover:text-gray-400"
+            aria-label="Submit prompt"
+            disabled={isLoading}
+          >
+            Generate
+          </button>
+        </section>
       </form>
+      <section className="w-full flex items-center justify-center p-5">
+        {isLoading && <Loader />}
+        {images.length === 0 && !isLoading && (
+          <>
+            <p className="text-white">No images generated yet</p>
+          </>
+        )}
+      </section>
     </>
   );
 };
